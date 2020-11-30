38d1d177d411724c0b93d63b71149f702184d669
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-30 17:43:52 -0600

    First Commit

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+const ComponentsScreen = () => {
+    return <Text style={styles.textStyle}>This is a ComponentScreen</Text>;
+};
+
+const ComponentsScreen2 = () => {
+    return <Text style={styles.textStyle}>This is a ComponentScreen</Text>;
+};
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 20
+    }
+});
+
+export default ComponentsScreen;
