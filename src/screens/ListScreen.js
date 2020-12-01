d453d703a0b29aa421ba2e42a2a049e795ae086e
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-30 19:09:47 -0600

    Lists

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,20 +3,25 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () => {
     const friends = [
-        {name: 'Avinash'},
-        {name: 'Akash'},
-        {name: 'Maddy'},
-        {name: 'Andy'}
+        {name: 'Avinash', age: 10},
+        {name: 'Akash', age: 20},
+        {name: 'Maddy', age: 30},
+        {name: 'Asndy', age: 40}
     ];
     return (
     <FlatList
+    keyExtractor = {(friend) => friend.name}
     data = {friends}
     renderItem = {({ item }) => {
-    return <Text>{item.name}</Text>
+    return <Text style = {styles.textStyle}>Name:{item.name} age:{item.age}</Text>;
     }}
     />);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 5
+    }
+});
 
 export default ListScreen;
