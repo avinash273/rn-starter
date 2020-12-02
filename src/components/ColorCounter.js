440d4dbaa50880191249f8fa36b68a2c338c6891
avinash273 <avinash.shanker@mavs.uta.edu>
2020-12-02 00:18:06 -0600

    RGB color pallete

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,16 +1,20 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
-const ColorCounter = ({colorName}) => {
+const ColorCounter = ({colorName, onIncrease, onDecrease}) => {
     return (
         <View>
-        <Text>{colorName}</Text>
-        <Button title={`Increase ${colorName}`} />
-        <Button title={`Decrease ${colorName}`} />
+        <Text style={styles.text}>{colorName}</Text>
+        <Button onPress={() => onIncrease()} title={`Increase ${colorName}`} />
+        <Button onPress={() => onDecrease()} title={`Decrease ${colorName}`} />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    text:{
+        fontSize: 20
+    }
+});
 
 export default ColorCounter;
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,17 +1,42 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 import ColorCounter from "../components/ColorCounter";
 
+const COLOR_INCREMENT = 10;
 const SquareScreen = () => {
+    const [red, setRed] = useState(0);
+    const [green, setGreen] = useState(0);
+    const [blue, setBlue] = useState(0);
+
+    console.log(red);
+
     return (
         <View>
-            <ColorCounter colorName="Red"/>
-            <ColorCounter colorName="Blue"/>
-            <ColorCounter colorName="Green"/>
+            <ColorCounter
+            onIncrease={() => setRed(red + COLOR_INCREMENT)}
+            onDecrease={() => setRed(red - COLOR_INCREMENT)}
+            colorName="Red"
+            />
+            <ColorCounter
+            onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+            onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+            colorName="Green"
+            />
+            <ColorCounter
+            onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+            onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+            colorName="Blue"
+            />
+
+            <View style={{height: 450, width: 500, backgroundColor: `rgb(${red}, ${green}, ${blue})`}} />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    text: {
+      fontSize: 25
+    }
+  });
 
 export default SquareScreen;
